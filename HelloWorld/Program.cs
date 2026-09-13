3d40170007b12fa68794df6917fdbbfba8a3a998
Tinashe <chavunduka@etsu.edu>
2026-09-13 14:48:59 -0400

    Add header comment and introduction lines

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -1,1 +1,10 @@
-﻿Console.WriteLine("Hello, World!");
+﻿/*
+* Name: Tinashe Chavunduka
+* Course: CSCI 1250, Section 001
+* Assignment: Lab 0, Hello World
+* Date: September 13, 2026
+* Description: Prints a greeting and a short introduction to the console.
+*/
+Console.WriteLine("Hello, World!");
+Console.WriteLine("My name is Tinashe.");
+Console.WriteLine("This is my first program at ETSU.");
